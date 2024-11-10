9c0f5ec8bb99d08fd4140be5e6188e1d155a9e79
kizukkk <zenro934@gmail.com>
2024-11-11 00:59:35 +0200

    added: user entity class

---
+++ src/olx-assistant-domain/User.cs
@@ -0,0 +1,12 @@
+﻿using olx_assistant_domain.Common;
+
+namespace olx_assistant_domain;
+public class User : BaseEntity
+{
+    public required string Login { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string Email { get; set; }
+    public required string TelegramUsername { get; set; }
+    public List<Target>? Targets { get; set; }
+
+}
