ee2b4ec61fa739106cdb112c2157a3732f7cdbc5
kizukkk <zenro934@gmail.com>
2024-11-11 00:59:13 +0200

    added: product entity class

---
+++ src/olx-assistant-domain/Product.cs
@@ -0,0 +1,15 @@
+﻿using olx_assistant_domain.Common;
+
+namespace olx_assistant_domain;
+public class Product : BaseEntity
+{
+    public int ProductId {  get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set;}
+    public string? Place { get; set;}
+    public decimal Price { get; set; }
+    public int Rating { get; set; }
+    public required Target SearchTarget { get; set; }
+    public List<Tag>? Tags { get; set; }
+
+}
