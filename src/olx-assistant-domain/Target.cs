07a80bf5852d8652fd78407ef523316e9c8e97fb
kizukkk <zenro934@gmail.com>
2024-11-11 00:59:24 +0200

    added: target entity class

---
+++ src/olx-assistant-domain/Target.cs
@@ -0,0 +1,9 @@
+﻿using olx_assistant_domain.Common;
+
+namespace olx_assistant_domain;
+public class Target : BaseEntity
+{
+    public required Uri TargetUri { get; set; }
+    public List<Keyword>? Keywords { get; set; }
+
+}
