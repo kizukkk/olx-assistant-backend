0f5fce24678a5d20902e45ce5ebe2338f0daa35b
kizukkk <zenro934@gmail.com>
2024-11-11 12:46:25 +0200

    added: test Target creation endpoint

---
+++ src/olx-assistant-api/Endpoints/Target/TargetCreate.cs
@@ -0,0 +1,21 @@
+﻿using olx_assistant_application.DTOs.Target;
+using FastEndpoints;
+
+namespace olx_assistant_api.Endpoints.Target;
+
+public class TargetCreate : Endpoint<TargetRequest>
+{
+    public override void Configure()
+    {
+        Post("/target/create");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(TargetRequest req, CancellationToken token)
+    {
+        await SendAsync(new TargetResponse()
+        {
+            TargetUri = new Uri(req.TargetUri),
+        });
+    }
+}
