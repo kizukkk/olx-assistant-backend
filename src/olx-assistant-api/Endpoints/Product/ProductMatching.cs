3bfc5a3ce0e3244412587ab8773a63d5238c63f2
kizukkk <zenro934@gmail.com>
2024-11-12 20:56:11 +0200

    added: product mathcing endpoint (template)

---
+++ src/olx-assistant-api/Endpoints/Product/ProductMatching.cs
@@ -0,0 +1,29 @@
+﻿using FastEndpoints;
+using olx_assistant_application.Services;
+
+namespace olx_assistant_api.Endpoints.Product;
+
+public class ProductMatching : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/product/matching");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var target = new olx_assistant_domain.Entities.Target()
+        {
+            TargetUri = new Uri("https://www.olx.ua/uk/transport/selhoztehnika"),
+        };
+
+        await ProductMatchingService.StartMatchingByTarget(target);
+
+        await SendAsync( new
+        {
+
+        });
+    }
+
+}
