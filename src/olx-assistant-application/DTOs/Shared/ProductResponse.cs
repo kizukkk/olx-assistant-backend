ff1b3cca260cd42924a447140231a73ae1d92c26
kizukkk <zenro934@gmail.com>
2024-11-12 20:55:15 +0200

    added: product repsonse DTO

---
+++ src/olx-assistant-application/DTOs/Shared/ProductResponse.cs
@@ -0,0 +1,13 @@
+﻿using olx_assistant_domain.Entities.Common;
+
+namespace olx_assistant_application.DTOs.Shared;
+public class ProductResponse
+{
+    public int ProductId { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Place { get; set; }
+    public decimal Price { get; set; } = 0;
+    public int Rating { get; set; } = 0;
+    public List<Tag>? Tags { get; set; }
+}
