edc7b496ac3b4959a1e25c66a8b84016f3e6bb5c
kizukkk <zenro934@gmail.com>
2024-11-12 17:30:51 +0200

    edited: remove Place field

--- src/olx-assistant-domain/Entities/Product.cs
+++ src/olx-assistant-domain/Entities/Product.cs
@@ -3,13 +3,12 @@
 namespace olx_assistant_domain.Entities;
 public class Product : BaseEntity
 {
-    public int ProductId { get; set; }
+    public required int ProductId { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
-    public string? Place { get; set; }
-    public decimal Price { get; set; }
-    public int Rating { get; set; }
-    public required Target SearchTarget { get; set; }
+    public decimal Price { get; set; } = decimal.Zero;
+    public int Rating { get; set; } = 0;
+    public Target? SearchTarget { get; set; }
     public List<Tag>? Tags { get; set; }
 
 }
