99aaae824b80c15558231ed77c71b5197110a83b
kizukkk <zenro934@gmail.com>
2024-11-12 20:54:44 +0200

    added: matching product by target template Service

---
+++ src/olx-assistant-application/Services/ProductMatchingService.cs
@@ -0,0 +1,18 @@
+﻿using olx_assistant_application.DTOs.Shared;
+using olx_assistant_domain.Entities;
+using olx_assistant_scraping;
+
+namespace olx_assistant_application.Services;
+public static class ProductMatchingService
+{
+
+    public static async Task<List<ProductResponse>> StartMatchingByTarget(Target target)
+    {
+        Uri paginatedUrl = new Uri($"{target.TargetUri}/?page={1}");
+
+        //TODO: save product list and map to ProductResponse using Mapper
+        await ProductScraping.GetProductListAsync(paginatedUrl);
+
+        return new List<ProductResponse>();
+    }
+}
