ef210d933be2637cd509022c661b377b257dfdbe
kizukkk <zenro934@gmail.com>
2024-11-15 13:50:05 +0200

    added: mapper profile with Product -> ProductResponse

---
+++ src/olx-assistant-application/Mapper/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using olx_assistant_application.DTOs.Shared;
+using olx_assistant_domain.Entities;
+
+namespace olx_assistant_application.Mapper;
+public class MapperProfile: Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<Product, ProductResponse>();
+    }
+}
