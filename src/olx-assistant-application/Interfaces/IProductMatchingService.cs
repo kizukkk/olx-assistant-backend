8d758335bb121827e57f2449d6a705d76340a9ab
kizukkk <zenro934@gmail.com>
2024-11-15 13:47:28 +0200

    added: Prodcut Mathching Service Interface

---
+++ src/olx-assistant-application/Interfaces/IProductMatchingService.cs
@@ -0,0 +1,8 @@
+﻿using olx_assistant_application.DTOs.Shared;
+using olx_assistant_domain.Entities;
+
+namespace olx_assistant_application.Interfaces;
+public interface IProductMatchingService
+{
+    public Task<List<ProductResponse>> StartMatchingByTargetAsync(Target target);
+}
