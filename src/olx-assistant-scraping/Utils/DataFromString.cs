cd50de8dafb85d679820cb02939ca4424455fa91
kizukkk <zenro934@gmail.com>
2024-11-15 12:23:59 +0200

    TODO : Process the date case "Сьогодні" (today) - fix

--- src/olx-assistant-scraping/Utils/DataFromString.cs
+++ src/olx-assistant-scraping/Utils/DataFromString.cs
@@ -5,7 +5,9 @@ public static class DataFromString
 {
     public static DateTime GetDateFromStr(string str)
     {
-        //TODO : Process the date case "Сьогодні" (today) 
+        // TODO : Process the date case "Сьогодні" (today) 
+        // Потрібно опрацювати подію коли Date вказана Сьогодні
+        // Наприклад використати Date.Now() або Date.UtcNow()
         str = str.Replace("р.", String.Empty).TrimEnd();
         DateTime date = DateTime.ParseExact(str, "d MMMM yyyy", new CultureInfo("uk-UA"));
         return date;
