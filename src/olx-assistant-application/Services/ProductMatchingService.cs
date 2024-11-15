55a5a58c3190f721f6dde45fe6ab440e8a4ef77f
kizukkk <zenro934@gmail.com>
2024-11-15 13:49:11 +0200

    added: IProductMatchingServices imple. & AutoMapper Product > ProductResponse

--- src/olx-assistant-application/Services/ProductMatchingService.cs
+++ src/olx-assistant-application/Services/ProductMatchingService.cs
@@ -1,19 +1,24 @@
 ﻿using olx_assistant_application.DTOs.Shared;
+using olx_assistant_application.Interfaces;
 using olx_assistant_domain.Entities;
 using olx_assistant_scraping;
+using AutoMapper;
 
 namespace olx_assistant_application.Services;
-public static class ProductMatchingService
+public class ProductMatchingService : IProductMatchingService
 {
+    private readonly IMapper _mapper;
 
-    public static async Task<List<ProductResponse>> StartMatchingByTarget(Target target)
+    public ProductMatchingService(IMapper mapper) => _mapper = mapper;
+
+    public async Task<List<ProductResponse>> StartMatchingByTargetAsync(Target target)
     {
         Uri paginatedUrl = new Uri($"{target.TargetUri}/?page={1}");
 
-        //TODO: save product list and map to ProductResponse using Mapper
         var scraper = new ProductsScraping(paginatedUrl);
         var products = await scraper.GetProductList();
+        var mappedProducts = _mapper.Map<List<Product>, List<ProductResponse>>(products);
 
-        return new List<ProductResponse>();
+        return mappedProducts;
     }
 }
