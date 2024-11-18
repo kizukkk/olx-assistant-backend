8f5dff650410d89b1cc7266ee97deeb072539350
kizukkk <zenro934@gmail.com>
2024-11-19 00:43:41 +0200

    addd: MSSQL database context

---
+++ src/olx-assistant-infrastructure/DbContexts/MsSqlDbContext.cs
@@ -0,0 +1,22 @@
+﻿using olx_assistant_domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using olx_assistant_domain.Entities.Common;
+
+namespace olx_assistant_infrastructure.DbContexts;
+public class MsSqlDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Target> Targets { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+
+    public MsSqlDbContext(DbContextOptions<MsSqlDbContext> options) 
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder model)
+    {
+        base.OnModelCreating(model);
+    }
+}
