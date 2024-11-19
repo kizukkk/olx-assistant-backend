d57ce0f0f674cad54078275728a2d14c9c35ccf9
kizukkk <zenro934@gmail.com>
2024-11-19 11:57:22 +0200

    added: product repo

---
+++ src/olx-assistant-infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using olx_assistant_application.Interfaces.IRepositories;
+using olx_assistant_infrastructure.DbContexts;
+using olx_assistant_domain.Entities;
+
+namespace olx_assistant_infrastructure.Repositories;
+public class ProductRepository : BaseRepository<Product>, IProductRepository
+{
+    private readonly MsSqlDbContext _context;
+
+    public ProductRepository(MsSqlDbContext context)
+        : base(context)
+    {
+        _context = context;
+    }
+
+}
