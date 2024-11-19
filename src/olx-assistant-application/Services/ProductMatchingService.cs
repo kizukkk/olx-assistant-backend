7e14694ec6de6cc5cf9474f2c732d125c5f7d9e6
kizukkk <zenro934@gmail.com>
2024-11-19 11:58:50 +0200

    added: saving data to db

--- src/olx-assistant-application/Services/ProductMatchingService.cs
+++ src/olx-assistant-application/Services/ProductMatchingService.cs
@@ -1,4 +1,5 @@
-﻿using olx_assistant_application.Interfaces.IServices;
+﻿using olx_assistant_application.Interfaces.IRepositories;
+using olx_assistant_application.Interfaces.IServices;
 using olx_assistant_application.DTOs.Shared;
 using olx_assistant_domain.Entities;
 using olx_assistant_scraping;
@@ -8,8 +9,16 @@ namespace olx_assistant_application.Services;
 public class ProductMatchingService : IProductMatchingService
 {
     private readonly IMapper _mapper;
+    private readonly IProductRepository _repo;
 
-    public ProductMatchingService(IMapper mapper) => _mapper = mapper;
+    public ProductMatchingService(
+        IMapper mapper, 
+        IProductRepository repository
+        ) 
+    {
+        _mapper = mapper;
+        _repo = repository;
+    }
 
     public async Task<List<ProductResponse>> StartMatchingByTargetAsync(Target target)
     {
@@ -18,6 +27,9 @@ public class ProductMatchingService : IProductMatchingService
         var scraper = new ProductsScraping(paginatedUrl);
         var products = await scraper.GetProductList();
         var mappedProducts = _mapper.Map<List<Product>, List<ProductResponse>>(products);
+
+        products.ForEach(item => _repo.Create(item));
+        _repo.SaveChanges();
 
         return mappedProducts;
     }
