59efc501468e8bdb3922bed893b05044b19c7046
kizukkk <zenro934@gmail.com>
2024-11-20 22:30:09 +0200

    added: cashing product id into Redis on app start & listening

--- src/olx-assistant-database-listener/Program.cs
+++ src/olx-assistant-database-listener/Program.cs
@@ -1,29 +1,78 @@
 ﻿using Microsoft.Data.SqlClient;
+using StackExchange.Redis;
 
-const string SqlConnectionString_Watch = "Server=AFRODITA\\LOCALSQLSERVER;Database=olx_assistance_db;Trusted_Connection=True;User Id=app_server;Password=202124; TrustServerCertificate=True";
-const string RedisConnectionString_Write = "write";
 
-SqlDependency.Start(SqlConnectionString_Watch);
+ListeningServices.RedisInit();
+ListeningServices.StartListening();
 
-using (SqlConnection connection = new SqlConnection(SqlConnectionString_Watch))
+
+static class ListeningServices
 {
-    connection.Open();
+    static readonly string SqlConnectionString = "Server=AFRODITA\\LOCALSQLSERVER;Database=olx_assistance_db;Trusted_Connection=True;User Id=app_server;Password=202124; TrustServerCertificate=True";
+    static readonly ConnectionMultiplexer _redis = ConnectionMultiplexer.Connect($"localhost:6379");
+    static readonly IDatabase db = _redis.GetDatabase();
 
-    Console.WriteLine("Notification Waiting...");
-    while (true)
+    public static void RedisInit()
     {
-        using (SqlCommand command = new SqlCommand(@"
+        db.KeyDelete("product_id");
+
+        using (var connection = new SqlConnection(SqlConnectionString))
+        {
+            connection.Open();
+
+            var sql = "SELECT [ProductId] FROM [olx_assistance_db].[dbo].[Products]";
+
+            using var command = new SqlCommand(sql, connection);
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int data = (int) reader["ProductId"];
+                db.SetAdd("product_id", data);
+                Console.WriteLine($"Cashing Product: {data}");
+            }
+            connection.Close();
+        }
+    }
+
+    public static void StartListening()
+    {
+        SqlDependency.Start(SqlConnectionString);
+
+        using (SqlConnection connection = new SqlConnection(SqlConnectionString))
+        {
+            connection.Open();
+
+            Console.WriteLine("Notification Waiting...");
+            while (true)
+            {
+                using (SqlCommand command = new SqlCommand(@"
             WAITFOR (
                 RECEIVE TOP(1)
                     CONVERT(NVARCHAR(MAX), message_body) AS MessageBody
                 FROM TargetQueue
             ), TIMEOUT 5000;", connection))
-        {
-            var result = command.ExecuteScalar();
-            if (result != null && !string.IsNullOrEmpty(result.ToString()))
-            {
-                Console.WriteLine($"Received a new record: {result}");
+                {
+                    var result = command.ExecuteScalar();
+                    if (result != null && !string.IsNullOrEmpty(result.ToString()))
+                    {
+                        int data = 0;
+
+                        try
+                        {
+                            data = int.Parse((string)result);
+                        }catch (Exception ex)
+                        {
+                            Console.WriteLine($"Failed Parse data from result - {result}");
+                            Console.WriteLine(ex);
+                        }
+
+                        db.SetAdd("product_id", data);
+                        Console.WriteLine($"Received a new record: {result}");
+                    }
+                }
             }
         }
     }
 }
+
