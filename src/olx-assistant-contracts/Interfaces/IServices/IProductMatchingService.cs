d2d77fedf5ea6732dff08dc204da667e564b2e61
kizukkk <zenro934@gmail.com>
2024-11-22 21:31:06 +0200

    edited: move Interfaces to Contracts layer from Application

--- src/olx-assistant-contracts/Interfaces/IServices/IProductMatchingService.cs
+++ src/olx-assistant-contracts/Interfaces/IServices/IProductMatchingService.cs
@@ -1,5 +1,4 @@
-﻿using olx_assistant_application.DTOs.Shared;
-using olx_assistant_domain.Entities;
+﻿using olx_assistant_domain.Entities;
 
 namespace olx_assistant_application.Interfaces.IServices;
 public interface IProductMatchingService
