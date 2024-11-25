a6b810dac965fce81b44c248abd4ca0e45c8a224
kizukkk <zenro934@gmail.com>
2024-11-25 13:39:33 +0200

    added: product cache service on Appliaction layer

---
+++ src/olx-assistant-application/Services/ProductCacheService.cs
@@ -0,0 +1,15 @@
+﻿using olx_assistant_application.Interfaces.IRepositories;
+
+namespace olx_assistant_application.Services;
+public class ProductCacheService
+{
+    private readonly IRedisCashedRepository _cashedRepository;
+
+    public ProductCacheService(IRedisCashedRepository repo) => 
+        _cashedRepository = repo;
+
+    public async Task<bool> ProductIsCached(int id)
+    {
+       return await _cashedRepository.ProductIsProcessed(id);
+    }
+}
