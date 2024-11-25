7c01406263a75d17e7e04d65f35156cf3f8cb6f0
kizukkk <zenro934@gmail.com>
2024-11-25 13:37:47 +0200

    added: Cache Service extension

---
+++ src/olx-assistant-infrastructure/ServiceExtensions/CacheServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using olx_assistant_application.Interfaces.IRepositories;
+using olx_assistant_infrastructure.DbContexts;
+using olx_assistant_infrastructure.Repositories;
+
+namespace olx_assistant_infrastructure.ServiceExtensions;
+public static class CacheServiceExtension
+{
+    public static void CacheServiceConfig(
+    this IServiceCollection services,
+    IConfiguration configuration
+    )
+    {
+        var connectionString = configuration.GetConnectionString("CacheService");
+
+        services.AddSingleton<IRedisCashedRepository>(
+            new RedisCashedRepository(connectionString));
+    }
+}
