4e4db552154a2c537479653a847547efe1036634
kizukkk <zenro934@gmail.com>
2024-11-26 20:18:31 +0200

    added: Config Cache services on Server pipline

--- src/olx-assistant-infrastructure/ServiceExtensions/CacheServiceExtension.cs
+++ src/olx-assistant-infrastructure/ServiceExtensions/CacheServiceExtension.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using olx_assistant_application.Interfaces.IRepositories;
-using olx_assistant_infrastructure.DbContexts;
+﻿using olx_assistant_application.Interfaces.IRepositories;
 using olx_assistant_infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Net.Security;
+using Grpc.Net.Client;
 
 namespace olx_assistant_infrastructure.ServiceExtensions;
 public static class CacheServiceExtension
@@ -14,7 +15,29 @@ public static class CacheServiceExtension
     {
         var connectionString = configuration.GetConnectionString("CacheService");
 
-        services.AddSingleton<IRedisCashedRepository>(
-            new RedisCashedRepository(connectionString));
+        var grpcChannel = GrpcChannel.ForAddress(connectionString, new GrpcChannelOptions
+        {
+            //LoggerFactory = LoggerFactory.Create(builder =>
+            //{
+            //    builder.AddConsole();
+            //    builder.SetMinimumLevel(LogLevel.Debug);
+            //}),
+            HttpHandler = new SocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(1),
+                KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+                KeepAlivePingPolicy = HttpKeepAlivePingPolicy.WithActiveRequests,
+                EnableMultipleHttp2Connections = true,
+                PooledConnectionIdleTimeout = TimeSpan.FromMinutes(5),
+                SslOptions = new SslClientAuthenticationOptions
+                {
+                    RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+                }
+            }
+        });
+
+        services.AddSingleton(grpcChannel);
+        services.AddSingleton(new isChased.isChasedClient(grpcChannel));
+        services.AddTransient<IRedisCashedRepository, RedisCashedRepository>();
     }
 }
