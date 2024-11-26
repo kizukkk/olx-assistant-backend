72bdfcf9577ced58c0df19a98202b900f5f5867f
kizukkk <zenro934@gmail.com>
2024-11-26 20:16:10 +0200

    added: config gRPC for using Redis DB

--- src/olx-assistant-cache-service/Program.cs
+++ src/olx-assistant-cache-service/Program.cs
@@ -1,9 +1,22 @@
 using olx_assistant_cache_service.Services;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+#region Application Service
+
 builder.Services.AddGrpc();
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var configuration = builder.Configuration.GetConnectionString("Redis");
+    return ConnectionMultiplexer.Connect(configuration);
+});
+
+#endregion
+
 
 var app = builder.Build();
 
