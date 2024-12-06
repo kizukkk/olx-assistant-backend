491e2c9e92ffdc3bbdb0ba745a95f16c208f40a9
kizukkk <zenro934@gmail.com>
2024-12-06 13:49:16 +0200

    edited: add field for proc. task id and remov TargetInitSearch id field

--- src/olx-assistant-domain/Entities/Product.cs
+++ src/olx-assistant-domain/Entities/Product.cs
@@ -4,11 +4,11 @@ namespace olx_assistant_domain.Entities;
 public class Product : BaseEntity
 {
     public int ProductId { get; set; }
+    public string? ProcessedByTaskId { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; } = decimal.Zero;
     public int Rating { get; set; } = 0;
-    public Target? SearchTarget { get; set; }
     public List<Tag>? Tags { get; set; }
 
 }
