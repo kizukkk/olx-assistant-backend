a9da560f85381859fac9e3a51d55876a33ecae4e
kizukkk <zenro934@gmail.com>
2024-12-06 13:50:13 +0200

    added: JobTarget Repo. Interface and implementation

---
+++ src/olx-assistant-contracts/Interfaces/IRepositories/IJobTargetRepository.cs
@@ -0,0 +1,7 @@
+﻿using olx_assistant_domain.Entities.Common;
+
+namespace olx_assistant_contracts.Interfaces.IRepositories;
+public interface IJobTargetRepository
+{
+    public void RegisterTask(TargetJob targetJob);
+}
---
+++ src/olx-assistant-infrastructure/Repositories/JobTargetRepository.cs
@@ -0,0 +1,20 @@
+﻿using olx_assistant_contracts.Interfaces.IRepositories;
+using olx_assistant_domain.Entities.Common;
+using olx_assistant_infrastructure.DbContexts;
+
+namespace olx_assistant_infrastructure.Repositories;
+public class JobTargetRepository : IJobTargetRepository
+{
+    private readonly MsSqlDbContext _context;
+
+    public JobTargetRepository(MsSqlDbContext context)
+    {
+        _context = context;
+    }
+    public void RegisterTask(TargetJob targetTask)
+    {
+        _context.TargetTasks.Add(targetTask);
+        _context.SaveChanges();
+    }
+
+}
