77921d9d7688caf1633ead7d81814206753e503f
kizukkk <zenro934@gmail.com>
2025-02-08 14:04:54 +0200

    added: DB migration (change int to float)

---
+++ src/olx-assistant-infrastructure/Migrations/20250207130733_ChnageProductRate2Float.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace olx_assistant_infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChnageProductRate2Float : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Rating",
+                table: "Products",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Value",
+                table: "Keywords",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Value",
+                table: "Keywords",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
